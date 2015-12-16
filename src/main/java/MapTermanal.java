7c060504b4fca29e44d0ff0ecd6f721c5abf8f82
bogdan <bb.japrog@gmail.com>
2015-12-16 02:20:16 +0200

    сделал приоритеты и мапу со значениями

--- src/main/java/MapTermanal.java
+++ src/main/java/MapTermanal.java
@@ -10,10 +10,15 @@ public class MapTermanal {
     private static Map<String, Integer> mapTerminalSymbol = new HashMap<String, Integer>();
 
     static {
-        mapTerminalSymbol.put("-", Priority.MINUS);
-        mapTerminalSymbol.put("+", Priority.PLUS);
+        mapTerminalSymbol.put(">", Priority.MORE);
+        mapTerminalSymbol.put("<", Priority.LESS);
+
         mapTerminalSymbol.put("/", Priority.DIVISION);
         mapTerminalSymbol.put("*", Priority.MULTIPLY);
+
+        mapTerminalSymbol.put("-", Priority.MINUS);
+        mapTerminalSymbol.put("+", Priority.PLUS);
+
         mapTerminalSymbol.put("while", Priority.WHILE);
         mapTerminalSymbol.put("for", Priority.FOR);
         mapTerminalSymbol.put("if", Priority.IF);
@@ -24,9 +29,6 @@ public class MapTermanal {
         mapTerminalSymbol.put("Integer", Priority.INTEGER);
         mapTerminalSymbol.put("Double", Priority.DOUBLE);
         mapTerminalSymbol.put("int", Priority.INT);
-        mapTerminalSymbol.put(">", Priority.MORE);
-        mapTerminalSymbol.put("<", Priority.LESS);
-        mapTerminalSymbol.put("<", Priority.LESS);
         mapTerminalSymbol.put("Math.sqrt", Priority.SQRT);
         mapTerminalSymbol.put("(", Priority.LEFT_PARENTHESIS);
         mapTerminalSymbol.put(")", Priority.RIGHT_PARENTHESIS);
--- src/main/java/Priority.java
+++ src/main/java/Priority.java
@@ -3,27 +3,32 @@
  */
 public class Priority {
 
-    public static final int MINUS = 1;
-    public static final int PLUS = 2;
+    public static final int LEFT_PARENTHESIS = 18;
+    public static final int RIGHT_PARENTHESIS = 19;
+
+    public static final int MORE = 9;
+    public static final int LESS = 8;
+
+    public static final int DIVISION = 13;
+    public static final int MULTIPLY = 14;
+
+    public static final int MINUS = 11;
+    public static final int PLUS = 12;
+
     public static final int WHILE = 3;
     public static final int FOR = 4;
     public static final int IF = 5;
     public static final int ELSE = 6;
     public static final int TRUE = 7;
-    public static final int FALSE = 8;
-    public static final int BOOLEAN = 9;
-    public static final int INTEGER = 10;
-    public static final int DOUBLE = 11;
-    public static final int INT = 12;
-    public static final int DIVISION = 13;
-    public static final int MULTIPLY = 14;
-    public static final int MORE = 15;
-    public static final int LESS = 16;
-    public static final int SQRT = 17;
-    public static final int LEFT_PARENTHESIS = 18;
-    public static final int RIGHT_PARENTHESIS = 19;
-    public static final int SEMICOLON = 20;
+    public static final int FALSE = 10;
+    public static final int BOOLEAN = 15;
+    public static final int INTEGER = 16;
+    public static final int DOUBLE = 17;
+    public static final int INT = 20;
 
-    public static final int MAX_PRIORITY = 21;
+    public static final int SQRT = 21;
+    public static final int SEMICOLON = 22;
+
+    public static final int MAX_PRIORITY = 23;
 
 }
