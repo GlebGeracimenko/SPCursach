4d5883c90c524cae0bed7ec26017c067dbb6ba2d
bogdan <bb.japrog@gmail.com>
2015-12-21 17:26:14 +0200

    рефакторинг строки

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
  */
 public class Test {
 
+
     public static void main(String[] args) {
         String s = "dad = b - asdb - 4234 * a * 2;";
         SplitString splitString = new SplitString();
